49d2aafc4497ff875667bda25a3013da5b6d70cc
Daria1911 <fetko1992@gmail.com>
2023-06-14 10:30:42 -0400

    lesson_1_2 add title

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -3,7 +3,7 @@ import React from "react";
 const AddTodoForm = () => {
     return (
         <form>
-            <label htmlFor="todoTitle"></label>
+            <label htmlFor="todoTitle">Title</label>
             <input type="text" id="todoTitle"/>
             <button type="submit">Add</button>
         </form>
